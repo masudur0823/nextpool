b24308195937fc2def7d0594d3f8064ad714a1eb
mahibulmasud <mahibulmasud@gmail.com>
2022-07-31 22:13:36 +0600

    edited

--- asstes/js/function.js
+++ asstes/js/function.js
@@ -23,3 +23,24 @@ function viewLess(){
     div.style.overflowY = 'hidden'
 }
 // viewAll function end
+
+// faq function start
+var faq = document.getElementsByClassName("faq-page");
+var i;
+
+for (i = 0; i < faq.length; i++) {
+    faq[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var body = this.nextElementSibling;
+        if (body.style.display === "block") {
+            body.style.display = "none";
+        } else {
+            body.style.display = "block";
+        }
+    });
+}
+// faq function end
